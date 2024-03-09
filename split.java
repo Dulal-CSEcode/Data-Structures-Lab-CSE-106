80467738e97c2b0fcee4560fa02177bd225d2690
MD Dulal Hossain <112533806+Dulal-CSEcode@users.noreply.github.com>
2024-03-09 22:57:31 +0600

    Delete split.java

--- split.java
+++
@@ -1,9 +0,0 @@
-public class split {
-    public static void main(String[] args) {
-        String s = "I#Love#Bangladesh";
-        String[] a = s.split("#+");//Back slash ke " "or \s or\\s evabe lekha jay
-        for (int index = 0; index < a.length; index++) {
-            System.out.println(a[index]);
-        }
-    }
-}
