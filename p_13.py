b4e9fc8b9f94723b8fa130adef9138c6df24209c
MD Dulal Hossain <112533806+Dulal-CSEcode@users.noreply.github.com>
2024-04-13 22:18:07 +0600

    Add files via upload

---
+++ p_13.py
@@ -0,0 +1,15 @@
+""" P_13 Coding Exercises for
+ Beginners in Python | 
+Exercise #3 | Program to add digits of a number """
+""" first_num=input("Enter first number:\n")#3
+second_num=input("Enter the second num:\n")#2
+print(type(second_num))
+sum=int(first_num)+int(second_num)
+print(sum)
+ """
+num_1=input("Enter a two digits numbers:")
+f_digit=num_1[0]
+s_digit=num_1[1]
+print(f_digit+s_digit)
+print(int(f_digit)+int(s_digit))
+
