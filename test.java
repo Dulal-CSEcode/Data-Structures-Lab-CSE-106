7fc9d844aac0911ae7a3925a48211d93068e6e63
MD Dulal Hossain <112533806+Dulal-CSEcode@users.noreply.github.com>
2024-04-19 00:51:22 +0600

    Add files via upload

---
+++ test.java
@@ -0,0 +1,12 @@
+// package main;
+ class Main{
+    public static void main(String[] args){
+        System.out.println("This is my first Java Program.");
+    }
+}
+// package main;
+// class HelloWorld {
+//     public static void main(String[] args) {
+//         System.out.println("Hello, World!");
+//     }
+// }
