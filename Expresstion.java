437ea941ca709d182330c4546f3e346ae2e65060
MD Dulal Hossain <112533806+Dulal-CSEcode@users.noreply.github.com>
2024-04-20 00:06:41 +0600

    Add files via upload

---
+++ Expresstion.java
@@ -0,0 +1,14 @@
+public class Expresstion {
+    public static void main(String[] args) {
+        // Expresion =Operators +Operands
+        //Operands   = Variables,numbers,quantity,values
+        //Operators  :+,-,=,*,/,%
+        //Class and object topics here discuss???
+        expression Even = new expression();
+        expression Odd = new expression();
+        System.out.println("the number is " + Odd.n);
+        Even.even();
+        Odd.odd();
+    }
+    
+}
