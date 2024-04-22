101e13ab9a28cc566a0ddde1bc334a4b0bea878a
MD Dulal Hossain <112533806+Dulal-CSEcode@users.noreply.github.com>
2024-04-23 00:48:35 +0600

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 15;
+        int sum = n1 + n2;
+        System.out.println(sum);
+
+    }
+}
